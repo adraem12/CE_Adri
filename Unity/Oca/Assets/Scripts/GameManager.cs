3944bcbd44fc417f46a03988516c280156f20dd0
adraem12 <adraem12@gmail.com>
2026-02-10 12:02:35 +0100

    Oca

--- Unity/Oca/Assets/Scripts/GameManager.cs
+++ Unity/Oca/Assets/Scripts/GameManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    int[] squareState;
+    public int[] squareState;
     int[] squareFunction;
     public RectTransform[] squareObjects;
     public RectTransform playerFigure, aiFigure;
     int round = 1;
     int turn = 0;
-    public int dice = 0;
+    public int playerDice = 0;
+    public int rivalDice = 0;
 
     private void Awake()
     {
@@ -21,12 +22,11 @@ public class GameManager : MonoBehaviour
             squareState[i] = 0;
         for (int i = 0; i < squareFunction.Length; i++)
             squareFunction[i] = 0;
-        squareState[0] = -1;
         //Teleports
         squareFunction[0] = 1;
         squareFunction[5] = 1;
         //Roll again
-        squareFunction[12] = 2;
+        squareFunction[11] = 2;
         squareFunction[17] = 2;
         //Move back 3 squares
         squareFunction[4] = -1;
@@ -35,7 +35,7 @@ public class GameManager : MonoBehaviour
         squareFunction[18] = -1;
         squareFunction[19] = -1;
         //Win
-        squareFunction[20] = 99;
+        squareFunction[20] = 98;
     }
 
     private void Start()
@@ -49,29 +49,58 @@ public class GameManager : MonoBehaviour
         bool gameEnd = false;
         while (!gameEnd) 
         {
-            yield return new WaitUntil(() => dice != 0);
+            yield return new WaitUntil(() => playerDice != 0);
             yield return new WaitForSeconds(1);
-            if (squareState[0] == -1)
+            DiceChecker(1, out int newSquare);
+            playerFigure.localPosition = squareObjects[newSquare].localPosition;
+            yield return new WaitForSeconds(1);
+            while (squareFunction[newSquare] != 0 || squareState[newSquare] != 0)
             {
-                squareState[0] = 2;
-                playerFigure.localPosition = squareObjects[dice].localPosition;
-                squareState[dice] = 1;
+                SpecialSquareChecker(1, newSquare, out int specialSquare);
+                if (specialSquare != newSquare)
+                    newSquare = specialSquare;
+                playerFigure.localPosition = squareObjects[newSquare].localPosition;
+                if (squareState[newSquare] != 0)
+                {
+                    playerDice = 0;
+                    UIManager.instance.MinusPlusButtonsEnabled(true);
+                    yield return new WaitUntil(() => playerDice != 0);
+                    newSquare += playerDice;
+                    UIManager.instance.MinusPlusButtonsEnabled(false);
+                    playerFigure.localPosition = squareObjects[newSquare].localPosition;
+                    yield return new WaitForSeconds(1);
+                }
             }
-            else
-                for (int i = 0; i < squareState.Length; i++)
-                    if (squareState[i] == 1)
-                    {
-                        squareState[i] = 0;
-                        if (i + dice < squareState.Length)
-                        {
-                            squareState[i + dice] = 1;
-                            playerFigure.localPosition = squareObjects[i + dice].localPosition;
-                        }
-                        break;
-                    }
-            dice = 0;
+            squareState[newSquare] = 1;
+            playerDice = 0;
             UIManager.instance.diceButton.interactable = true;
         }
+    }
+
+    void DiceChecker(int player, out int newSquare)
+    {
+        newSquare = 0;
+        if (squareFunction[20] == 98)
+        {
+            squareFunction[20] = 99;
+            newSquare = playerDice - 1;
+        }
+        else
+            for (int i = 0; i < squareState.Length; i++)
+                if (squareState[i] == player)
+                {
+                    squareState[i] = 0;
+                    if (i + playerDice < squareState.Length)
+                        newSquare = i + playerDice;
+                    else
+                        newSquare = i - (playerDice - (squareState.Length - i));
+                    break;
+                }
+    }
+
+    void SpecialSquareChecker(int player, int oldSquare, out int newSquare)
+    {
+        newSquare = oldSquare;
     }
 
     public static int Dice()
--- Unity/Oca/Assets/Scripts/UIManager.cs
+++ Unity/Oca/Assets/Scripts/UIManager.cs
@@ -6,7 +6,7 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
     public TextMeshProUGUI roundText, turnText, diceText;
-    public Button diceButton;
+    public Button diceButton, minusButton, plusButton;
 
     private void Awake()
     {
@@ -21,10 +21,21 @@ public class UIManager : MonoBehaviour
         diceText.text = dice.ToString();
     }
 
+    public void MinusPlusButtonsEnabled(bool enabled)
+    {
+        minusButton.gameObject.SetActive(enabled);
+        plusButton.gameObject.SetActive(enabled);
+    }
+
     public void DiceButton()
     {
         int a = GameManager.Dice();
-        GameManager.instance.dice = a;
+        GameManager.instance.playerDice = a;
         diceText.text = a.ToString();
+    }
+
+    public void CollisionButton(int i)
+    {
+        GameManager.instance.playerDice = i;
     }
 }
